aa3f786f3ba6032bb7940bf8479326955fef3703
scsa1987 <scsa1987@gmail.com>
2024-07-26 14:57:14 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+
+ access_key = "AKIAW5IZRWOYXBT7OQIL"
+
+ secret_key = "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"
+
+ region = "us-east-1"
+
+
+
+}
+
+
+
+
+
+resource "aws_vpc" "dep1" {
+
+  cidr_block = "192.168.0.0/16"
+
+  tags = {
+
+   "Name" = "VPC1"
+
+    
+
+  }
